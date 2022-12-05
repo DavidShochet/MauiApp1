d8a892821dae35862d632b47b8bbce31fbfa8f6a
Dmitriy Reznik <dreznik@laticrete.com>
2022-12-05 10:17:36 -0500

    Add existing project files to Git

---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage(MainPageModel pageModel)
+	{
+		InitializeComponent();
+        BindingContext = pageModel;
+    }
+}
+
---
+++ MainPageModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui;
+
+namespace MauiApp1
+{
+    public partial class MainPageModel : ObservableValidator
+    {
+        [ICommand]
+        private async Task ShareAsync()
+        {
+            string text = $"Some Text to Share";
+
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Text = text,
+                Title = "Calculation Results"
+            });
+        }
+    }
+}
